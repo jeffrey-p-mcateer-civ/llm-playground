587412bfe3e69705cfce9a580ebc22d337cd517c
Jeffrey McAteer <jeffrey.p.mcateer.civ@mail.mil>
2023-11-20 09:13:26 -0500

    Excellent test-case for a very tiny BERT model trained for question answering

--- run.py
+++ run.py
@@ -30,50 +30,56 @@ def main(args=sys.argv):
     model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
     tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
 
-    question = '''What is Machine Learning?'''
+    question = '''What color is car 3?'''
 
-    paragraph = ''' Machine learning (ML) is the scientific study of algorithms and statistical models that computer systems use to progressively improve their performance 
-                    on a specific task. Machine learning algorithms build a mathematical model of sample data, known as "training data", in order to make predictions or 
-                    decisions without being explicitly programmed to perform the task. Machine learning algorithms are used in the applications of email filtering, detection 
-                    of network intruders, and computer vision, where it is infeasible to develop an algorithm of specific instructions for performing the task. Machine learning 
-                    is closely related to computational statistics, which focuses on making predictions using computers. The study of mathematical optimization delivers methods, 
-                    theory and application domains to the field of machine learning. Data mining is a field of study within machine learning, and focuses on exploratory 
-                    data analysis through unsupervised learning.In its application across business problems, machine learning is also referred to as predictive analytics. '''
-                
-    encoding = tokenizer.encode_plus(text=question,text_pair=paragraph)
+    questions_and_answer = [
+        ('What color is car 1?', 'blue'),
+        ('What kind of car is car 1?', 'sedan'),
+        ('What color is car 2?', 'white'),
+        ('What kind of car is car 2?', 'Jeep'),
+        ('What color is car 3?', 'black'),
+        ('What color is car 4?', 'green'),
+    ]
 
-    inputs = encoding['input_ids']  #Token embeddings
-    sentence_embedding = encoding['token_type_ids']  #Segment embeddings
-    tokens = tokenizer.convert_ids_to_tokens(inputs) #input tokens
-
-    #start_scores, end_scores = model(input_ids=torch.tensor([inputs]), token_type_ids=torch.tensor([sentence_embedding]))
-    start_scores, end_scores = model(torch.tensor([inputs]),token_type_ids=torch.tensor([sentence_embedding]), return_dict=False)
-
-    print(f'start_scores = {start_scores}  end_scores = {end_scores}')
-
-    start_index = torch.argmax(start_scores)
-
-    end_index = torch.argmax(end_scores)
-
-    answer = ' '.join(tokens[start_index:end_index+1])
-
-    corrected_answer = ''
-
-    for word in answer.split():
-        
-        #If it's a subword token
-        if word[0:2] == '##':
-            corrected_answer += word[2:]
-        else:
-            corrected_answer += ' ' + word
-
+    paragraph = ''' There is a park with five cars parked. The first car is a blue sedan. The second car is a Jeep colored white. The third car is 
+                        is a black van with baloons attached to the top. Car number four is a green sedan. There are trees surrounding the cars.
+        '''
+    
     print()
     print(f'paragraph = {paragraph}')
-    print()
-    print(f'question = {question}')
-    print()
-    print(f'corrected_answer = {corrected_answer}')
-    print()
+        
+    for question, expected_answer in questions_and_answer:
+                    
+        encoding = tokenizer.encode_plus(text=question,text_pair=paragraph)
+
+        inputs = encoding['input_ids']  #Token embeddings
+        sentence_embedding = encoding['token_type_ids']  #Segment embeddings
+        tokens = tokenizer.convert_ids_to_tokens(inputs) #input tokens
+
+        #start_scores, end_scores = model(input_ids=torch.tensor([inputs]), token_type_ids=torch.tensor([sentence_embedding]))
+        start_scores, end_scores = model(torch.tensor([inputs]),token_type_ids=torch.tensor([sentence_embedding]), return_dict=False)
+
+        start_index = torch.argmax(start_scores)
+
+        end_index = torch.argmax(end_scores)
+
+        answer = ' '.join(tokens[start_index:end_index+1])
+
+        corrected_answer = ''
+
+        for word in answer.split():
+            
+            #If it's a subword token
+            if word[0:2] == '##':
+                corrected_answer += word[2:]
+            else:
+                corrected_answer += ' ' + word
+
+        print()
+        print(f'question = {question}')
+        print(f'corrected_answer = {corrected_answer}')
+        print(f'expected_answer = {expected_answer}')
+        print()
     
 
 
